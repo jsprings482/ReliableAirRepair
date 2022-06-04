b6c9a99e3c4167e4aeaa406cd73af57bb8cf9bd2
actech482 <jefferysprings482@gmail.com>
2022-06-04 12:13:00 -0500

    ...

--- RAR/RAR/wsgi.py
+++ RAR/RAR/wsgi.py
@@ -12,7 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RAR.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application, root=os.path.join(BASE_DIR, "static"))
