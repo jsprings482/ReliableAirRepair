f89fafef356cda210e8fc970cf639c49e5575cc9
actech482 <jefferysprings482@gmail.com>
2022-06-04 23:14:54 -0500

    wsgi

--- RAR/RAR/wsgi.py
+++ RAR/RAR/wsgi.py
@@ -10,11 +10,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RAR.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application, root=os.path.join(BASE_DIR, "static"))
