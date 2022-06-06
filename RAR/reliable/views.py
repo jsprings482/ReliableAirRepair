dfcbe18e2129ae08237ac7db119d42189e2993b3
actech482 <jefferysprings482@gmail.com>
2022-06-06 12:59:55 -0500

    pricing

--- RAR/reliable/views.py
+++ RAR/reliable/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'reliable/index.html')
+
+def pricing(request):
+    return render(request, 'reliable/pricing.html')
+
