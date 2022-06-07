f147396a17ee049197911243a4b04fc75e1ff0fd
actech482 <jefferysprings482@gmail.com>
2022-06-07 04:25:11 -0500

    users

--- RAR/reliable/views.py
+++ RAR/reliable/views.py
@@ -11,7 +11,7 @@ from django.db.models.query_utils import Q
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
-from .models import service
+from .models import service_call
 from datetime import datetime
 
 # Create your views here.
