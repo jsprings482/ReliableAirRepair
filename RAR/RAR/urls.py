acf225d3d843bdeba39467f07a342c0cd81f4826
actech482 <jefferysprings482@gmail.com>
2022-06-06 20:18:52 -0500

    users

--- RAR/RAR/urls.py
+++ RAR/RAR/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path( '', include( 'reliable.urls' ), name='home'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='reliable/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='reliable/password_reset_confirmation.html', name='password_reset_confirm'),
-    path'reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='reliable/password_reset_complete.html', name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='reliable/password_reset_complete.html', name='password_reset_complete'),
 ]
