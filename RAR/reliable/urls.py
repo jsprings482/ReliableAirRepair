b46e38ffb9b317e4437f3b02e3902a99c07e201c
actech482 <jefferysprings482@gmail.com>
2022-06-06 21:26:48 -0500

    users

--- RAR/reliable/urls.py
+++ RAR/reliable/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),        
+    path('/', views.index, name='index'),        
     path('pricing/', views.pricing, name='pricing'),
     path('register/', views.register, name= 'register'),
     path('logon/', views.logon, name= 'logon'),
