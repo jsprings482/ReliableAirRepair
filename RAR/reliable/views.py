850d5fbfab2f5fc6ce17521aab05a028c44f241b
actech482 <jefferysprings482@gmail.com>
2022-06-10 21:05:33 -0500

    ll

--- RAR/reliable/views.py
+++ RAR/reliable/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from .forms import NewUserForm, RequestForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages, auth
@@ -14,7 +14,6 @@ from django.utils.encoding import force_bytes
 from .models import service_call
 from datetime import datetime
 import os, requests
-from django.urls import reverse
 
 # Create your views here.
 def index(request):
